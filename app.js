4ba7cabaa6e2173ed9b9571254cb7114af1115d9
emsv7708 <emilija.svirskyte@mif.stud.vu.lt>
2022-07-18 18:53:12 +0300

    changed navbar with background

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+function waitAndChangeBackground() {
+    let background = document.getElementById('background');
+    let number = Math.floor(Math.random() * 3).toString(); //from 0 to 2
+    background.style.backgroundImage = 'url("images/city/city_4_'+number+'.png")'; // + number;        
+}
+setInterval(waitAndChangeBackground, 4000);
