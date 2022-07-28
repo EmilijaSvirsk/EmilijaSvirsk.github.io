62373c468eea759f6397577f78a1a3eaa8643ba4
emsv7708 <emilija.svirskyte@mif.stud.vu.lt>
2022-07-28 18:08:31 +0300

    fixed navbar buttons and added scrolling

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//mobile transformation
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
@@ -6,9 +7,26 @@ menu.addEventListener('click', function(){
     menuLinks.classList.toggle('active');
 });
 
+//bar city light flickering
 function waitAndChangeBackground() {
-    let background = document.getElementById('background');
+    let background = document.getElementById('background2');
     let number = Math.floor(Math.random() * 3).toString(); //from 0 to 2
     background.style.backgroundImage = 'url("images/city/city_4_'+number+'.png")'; // + number;        
 }
 setInterval(waitAndChangeBackground, 4000);
+
+//move bar city up
+
+let back1 = document.getElementById('background1');
+let back2 = document.getElementById('background2');
+let city = document.getElementById('city');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    if(value<45)
+    {
+        back2.style.top = value * -0.5 + 'px';
+        back1.style.top = value * -0.5 + 'px';
+        city.style.top = value * -0.2 + 'px';
+    }
+    })
